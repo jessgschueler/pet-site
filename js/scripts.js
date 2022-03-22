6f9a1840e44c1961d59923c1aab03eec34b2e17d
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-03-22 14:41:16 -0700

    Add light/dark mode and clickable highlight
    Co-Authored-by: Mike Wright <m.wright92@icloud.com>
    Co-authored-by: Meron Tekie <mtekiesd210309@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+  $("button#light").click(function() {
+    $("body").removeClass();
+  });
+  $(".highlight").click(function() {
+    $("ul").addClass("changeColor");
+  });
+});
